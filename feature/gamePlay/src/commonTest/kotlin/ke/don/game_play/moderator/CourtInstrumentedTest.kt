9f6a8b1b5d45469ede398d62714338dff30a4061
donald-okara <donaldokara123@gmail.com>
2025-12-12 21:57:01 +0300

    feat(game-play): Add TODOs for Court and Game Over tests
    * Adds a new test file `CourtInstrumentedTest`.
    * Includes TODOs for `Court` and `Game Over` instrumented tests.

---
+++ feature/gamePlay/src/commonTest/kotlin/ke/don/game_play/moderator/CourtInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package ke.don.game_play.moderator
+
+class CourtInstrumentedTest {
+    /**
+     * TODO()
+     * Test cases for *Court*
+     * * "Proceed" is *displayed*
+     * * "Proceed" is *disabled* if not all players have voted
+     * * "Proceed" moves to Game Over
+     */
+
+    /**
+     * TODO()
+     * Test cases for *Game Over*
+     * * "Play again" is *displayed* for moderator
+     * * Winning faction name is displayed
+     * * "Play again" moves to sleep
+     */
+}
