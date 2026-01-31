1219200b6a038791446a5d2a3f9c0ea25d8dd5b1
donald-okara <donaldokara123@gmail.com>
2026-01-31 16:57:43 +0300

    run spotless apply

--- feature/profile/src/androidMain/kotlin/ke/don/profile/EditProfilePreview.kt
+++ feature/profile/src/androidMain/kotlin/ke/don/profile/EditProfilePreview.kt
@@ -39,7 +39,7 @@ fun OnboardingShowcase(
     DevicePreviewContainer(theme) {
         val (state, handleEvent) = rememberEditProfileState()
 
-         OnBoardingContent(
+        OnBoardingContent(
             modifier = modifier,
             state = state,
             handleEvent = handleEvent,
